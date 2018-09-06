c7806e92269c4749afb59047e3981fe9b75cc1f9
Florian <florian@flova.de>
2018-09-06 19:36:22 +0200

    Contains first working logger

--- src/Plotter/run.py
+++ src/Plotter/run.py
@@ -1,12 +1,23 @@
 import serial 
+import matplotlib.pyplot as plt
 import sys
+import numpy as np
+import json
+import time
 
+PATH = ""
 
 def main():
     PORT="/dev/ttyACM0"
-    
+    TIME=300
+    SAVE = True
+    datenarray = list()
+
     if len(sys.argv) > 1:
         PORT = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        TIME = sys.argv[2]
 
     print("Running data logger at port {}.".format(PORT))
 
@@ -17,14 +28,29 @@ def main():
         except Exception as e:
             print ("Error open serial port: " + str(e))
         
-        while True:
+        for i in range(0,TIME):
             try:
                 data = parse(ser.readline())
                 if data:
                     int_value = data
+                    datenarray.append(int_value)
                 print(int_value)
             except Exception as e:
                 print ("Error while communicating with Arduino : " + str(e))
+        
+        if SAVE:
+            save(datenarray)
+
+        plt.plot(datenarray)
+        plt.xlabel('time (s)')
+        plt.ylabel('Acceleration')
+        plt.title('')
+        plt.grid(True)
+        plt.show()
+
+def save(data):
+    with open(PATH + 'rec' + str(time.time()) + '.json', 'w') as outfile:
+        json.dump(data, outfile)
 
 def parse(bytes):
     if len(bytes) == 4:
